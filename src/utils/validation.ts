7d91ab7e55107e32da02447bd9813aed373d070d
KennethJT1 <oluwatomiwa96@gmail.com>
2022-12-12 11:00:10 +0100

    initial

---
+++ src/utils/validation.ts
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+//Riders login
+export const loginSchema = Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')) 
+});
+
+export const option = {
+    abortearly: false,
+    errors:{
+        wrap:{
+            label:''
+        }
+    }
+};
