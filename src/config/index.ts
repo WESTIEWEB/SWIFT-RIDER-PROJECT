f0274662dd7767983121691655ca64b590d361e7
Deyems <adeyemisola11@gmail.com>
2022-12-13 08:53:16 +0100

    changing the setup for postgres

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,9 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = new Sequelize('app', '', '', {
-    storage: "./rider.postgres",
+export const db = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
+    host: "localhost",
+    port: Number(process.env.DB_PORT),
     dialect: "postgres",
     logging: false
 });
