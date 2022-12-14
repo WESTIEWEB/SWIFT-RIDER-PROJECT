5d07b686e67d6693036fce5387a4b1aefbecddbf
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2022-12-14 08:26:36 +0100

    signin jwt modifications

--- src/routes/rider.ts
+++ src/routes/rider.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import {login} from "../controller/riderController"
-import {registerRider} from "../controller/riderController"
+import {registerRider} from "../controller/riderController";
+// import { upload } from '../utils/multer'
 
 import multer from 'multer';
 const upload = multer({dest: 'uploads/'})
