bc2e4bce3fa166eaeef180fbceeb76e8f8665cf3
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2022-12-14 16:36:41 +0100

    duplicate code removed from riders model

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,19 +1,16 @@
 import Joi from 'joi';
-
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
-import {UserPayload, APP_SECRET} from '../config'
-import { AccountSid, authToken, fromAdminPhone } from '../config';
 dotenv.config()
 
 import jwt,{JwtPayload} from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import { APP_SECRET } from '../config/index';
 import { AuthPayload } from '../interface/Auth.dto';
 
 ///rider signup
-export const registerSchema = Joi.object().keys({
+export const riderRegisterSchema = Joi.object().keys({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     email: Joi.string().required(),
@@ -62,48 +59,14 @@ export const GeneratePassword = async(password:string, salt:string)=>{
     return await bcrypt.hash(password, salt)
 
 }
-export const GenerateSignature = async (payload:UserPayload)=>{
+export const GenerateSignature = async (payload:AuthPayload)=>{
     
         return jwt.sign(payload, APP_SECRET, {expiresIn:'1d'})
         
 }
-export const GenerateOTP = () => {
-    const otp = Math.floor(1000 + Math.random() * 90000);
-    const expiry = new Date();
-    expiry.setTime(new Date().getTime() + 30 * 60 * 1000);
-    return { otp, expiry };
-    // console.log(otp, expiry)
-  };
-  // 
-  export const onRequestOTP = async (otp: number, toPhoneNumber: string) => {
-  //   console.log(toPhoneNumber, otp, fromAdminPhone, AccountSid, authToken);
-  
-    try {
-      const client = require("twilio")(AccountSid, authToken);
-      const response = await client.messages.create({
-        body: `Your OTP is ${otp}`,
-        to: toPhoneNumber,
-        from: fromAdminPhone,
-      });
-      return response;
-    } catch (error) {
-      console.log(error);
-      return "error sending otp";
-    }
-  };
-
-
-export const GenerateSalt = async()=>{
-    return await bcrypt.genSalt()
-}
-export const GeneratePassword = async(password:string, salt:string)=>{
-    return await bcrypt.hash(password,salt)
-}
 
 //GENERATE TOKEN FOR A USER
-export const GenerateSignature = async(payload:AuthPayload)=>{
-    return jwt.sign(payload, APP_SECRET,{expiresIn:'1d'})
-}
+
 
 export const verifySignature=async(signature:string)=>{
     return jwt.verify(signature, APP_SECRET) as JwtPayload  
