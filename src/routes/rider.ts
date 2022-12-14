2cd41cb16ce553890f5275d82b67e12f292c1c2a
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2022-12-14 16:39:09 +0100

    duplicate code removed from rider routes

--- src/routes/rider.ts
+++ src/routes/rider.ts
@@ -1,18 +1,16 @@
 import express, {Request, Response} from 'express';
-
-// import {login} from "../controller/riderController"
-
-const router = express.Router();
-
-
 import {login} from "../controller/riderController"
 import {registerRider} from "../controller/riderController";
 // import { upload } from '../utils/multer'
 
+const router = express.Router();
+
+
+
 import multer from 'multer';
 const upload = multer({dest: 'uploads/'})
 
-const router = express.Router();
+// const router = express.Router();
 
 router.post('/login', login)
 router.post('/signup', upload.single('image'), registerRider) 
