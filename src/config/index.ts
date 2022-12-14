9850b0e250be3909928ba801a69b2d733a0957f0
KennethJT1 <oluwatomiwa96@gmail.com>
2022-12-14 08:31:32 +0100

    delete db from config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 import {Sequelize} from 'sequelize';
-import dotenv from 'dotenv';
-//export * from "./db"  
+import dotenv from 'dotenv'; 
 dotenv.config();
 
 export const db = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
