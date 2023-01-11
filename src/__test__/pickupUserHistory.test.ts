4d7870123c8bdf62e396a7662d01afb734286ee2
KennethJT1 <112853926+KennethJT1@users.noreply.github.com>
2023-01-11 07:15:52 +0100

    Fixed pickup user  History test

--- src/__test__/pickupUserHistory.test.ts
+++ src/__test__/pickupUserHistory.test.ts
@@ -4,24 +4,34 @@ import app from "../app";
 
 const request = supertest(app);
 
-beforeAll(async () => {
-  await db.sync().then(() => console.log("Database is connected"));
-});
-
+//Test to get all orders for users
 describe("Testing pickup user history", () => {
-  it("When wrong userId is used", async () => {
-    //Arrange and act
-    const res = await request.get("/my-orders");
 
-    expect(res.status).toBe(404);
-  });
-});
+  beforeEach(async () => {
+    await db.sync().then(() => console.log("Database is connected")).catch(err=>{
+      console.log("Database is not connected")
+    });
+  })
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNhOWFiZDNmLThjYWQtNGQyZi05YjViLTM3N2VmMmRiM2VjNCIsImVtYWlsIjoidGVuQHRlbi5jb20iLCJ2ZXJpZmllZCI6ZmFsc2UsImlhdCI6MTY3MzM4NTczNSwiZXhwIjoxNjczNDcyMTM1fQ.lb9bT6FjuLezaErhtPbrKuTwRo1Us8Lv2X-Uo6gXlcc"
+  const wrongToken = "";
 
-describe("Testing pickup user history", () => {
-  it("Order details should contain an array", async () => {
-    //Arrange and act
-    const res = await request.get("/my-orders");
+  it('When wrong token is passed', async () => {
+    await request
+    .get("/users/my-orders")
+      .expect(401)
+      .set({ authorization: `Bearer ${wrongToken}` })
+      .then((response: any) => {
+        expect(response.status).toBe(401)
+      })
+  })
 
-    expect(res).toEqual(expect.arrayContaining([]));
-  });
+  it('When right token is passed', async () => {
+    await request
+    .get("/users/my-orders")
+      .expect(200)
+      .set({ authorization: `Bearer ${token}` })
+      .then((response: any) => {
+        expect(response.body).toHaveProperty("message")
+      })
+  })
 });
