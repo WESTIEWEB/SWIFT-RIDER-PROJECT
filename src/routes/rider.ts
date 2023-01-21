cd614ed576ab3d5acde06a401a97dee377f453bf
Hillariousice <107556065+Hillariousice@users.noreply.github.com>
2023-01-21 16:37:30 +0100

    rider register-controller modified

--- src/routes/rider.ts
+++ src/routes/rider.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerRider, updateRiderProfile, VerifyUser, ResendOTP, getAllBiddings, acceptBid, RiderHistory, getRiderProfile, getUserOrderById, getOrderOwnerNameById, DeliveryOtp, DeliveryResendOTP} from "../controller/riderController"
+import { registerRider, updateRiderProfile, VerifyUser, ResendOTP, getAllBiddings, acceptBid, RiderHistory, getRiderProfile, getUserOrderById, getOrderOwnerNameById} from "../controller/riderController"
 import { authRider } from '../middleware/authorization'
 import { upload } from '../utils/multer'
 
@@ -13,8 +13,6 @@ router.patch('/update-rider/:signature', authRider, updateRiderProfile)
 router.post('/verify/:signature', VerifyUser)
 router.get('/resend-otp/:signature', ResendOTP)
 
-router.post('/delivery-verify/:signature', DeliveryOtp)
-router.get('/delivery-resend-otp/:signature', DeliveryResendOTP)
 
 router.get('/rider-order-profile/:riderId', getRiderProfile)
 router.get("/all-biddings", getAllBiddings);
